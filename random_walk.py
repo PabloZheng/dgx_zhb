a21afc5542aab3e16e1d115ad869d1d086f7d74d
Pablo <920882204@qq.com>
2021-08-23 15:18:13 +0800

    '8.23'

--- random_walk.py
+++ random_walk.py
@@ -4,6 +4,30 @@ import numpy as np
 import os
 
 
+def marker_only(read_p, mark_p):
+    img = img_as_float(io.imread(read_p))
+
+    # 保留边缘的前提下进行去噪
+    from skimage.restoration import denoise_nl_means, estimate_sigma
+
+    sigma_est = np.mean(estimate_sigma(img, multichannel=True))
+    denoise_img = denoise_nl_means(img, h=1.15 * sigma_est, fast_mode=True,
+                                   patch_size=5, patch_distance=3, multichannel=True)
+
+    from skimage import exposure
+    eq_img = exposure.equalize_adapthist(denoise_img)
+    markers = np.zeros(img.shape, dtype=np.uint)
+
+    markers[(eq_img < 0.8) & (eq_img > 0.7)] = 1
+    markers[(eq_img > 0.85) & (eq_img < 0.99)] = 2
+
+    markers = np.zeros(img.shape, dtype=np.uint8)
+    from skimage.segmentation import random_walker
+    labels = random_walker(eq_img, markers, beta=10, mode='bf')
+    plt.imsave(mark_p, markers)
+
+
+
 def randomwalk(read_p, mark_p, seg_p):
     img = img_as_float(io.imread(read_p))
     # 画histogram，img.flat将图像展开成一维
@@ -37,7 +61,7 @@ def randomwalk(read_p, mark_p, seg_p):
 
     # The range of the binary image spans over (0, 1).
     # For markers, let us include all between each peak.
-    markers = np.zeros(img.shape, dtype=np.uint)
+    markers = np.zeros(img.shape, dtype=np.uint8)
 
     markers[(eq_img < 0.8) & (eq_img > 0.7)] = 1
     markers[(eq_img > 0.85) & (eq_img < 0.99)] = 2
@@ -47,35 +71,37 @@ def randomwalk(read_p, mark_p, seg_p):
     # https://scikit-image.org/docs/dev/api/skimage.segmentation.html#skimage.segmentation.random_walker
     labels = random_walker(eq_img, markers, beta=10, mode='bf')
     plt.imsave(mark_p, markers)
-    segm1 = (labels == 1)
-    segm2 = (labels == 2)
-    all_segments = np.zeros((eq_img.shape[0], eq_img.shape[1], 3))  # nothing but denoise img size but blank
-
-    all_segments[segm1] = (1, 0, 0)
-    all_segments[segm2] = (0, 1, 0)
-
-    # plt.imshow(all_segments)
-
-    from scipy import ndimage as nd
-
-    segm1_closed = nd.binary_closing(segm1, np.ones((3, 3)))
-    segm2_closed = nd.binary_closing(segm2, np.ones((3, 3)))
-
-    all_segments_cleaned = np.zeros((eq_img.shape[0], eq_img.shape[1], 3))
-
-    all_segments_cleaned[segm1_closed] = (1, 0, 0)
-    all_segments_cleaned[segm2_closed] = (0, 1, 0)
-
-    plt.imshow(all_segments_cleaned)
-    plt.imsave(seg_p, all_segments_cleaned)
+    # segm1 = (labels == 1)
+    # segm2 = (labels == 2)
+    # all_segments = np.zeros((eq_img.shape[0], eq_img.shape[1], 3))  # nothing but denoise img size but blank
+    #
+    # all_segments[segm1] = (1, 0, 0)
+    # all_segments[segm2] = (0, 1, 0)
+    #
+    # # plt.imshow(all_segments)
+    #
+    # from scipy import ndimage as nd
+    #
+    # segm1_closed = nd.binary_closing(segm1, np.ones((3, 3)))
+    # segm2_closed = nd.binary_closing(segm2, np.ones((3, 3)))
+    #
+    # all_segments_cleaned = np.zeros((eq_img.shape[0], eq_img.shape[1], 3))
+    #
+    # all_segments_cleaned[segm1_closed] = (1, 0, 0)
+    # all_segments_cleaned[segm2_closed] = (0, 1, 0)
+    #
+    # plt.imshow(all_segments_cleaned)
+    # plt.imsave(seg_p, all_segments_cleaned)
 
 
 if __name__ == '__main__':
     filename = os.listdir('I://dataset//EUS_bulk//train//P')
-    for i, name in enumerate(filename):
-        read_path = f'I://dataset//EUS_bulk//train//P//{name}'
-        marker1_path = f'I://dataset//marker//marker_{name}'
-        seg_path = f'I://dataset//segment//segment_{name}'
-        randomwalk(read_path, marker1_path, seg_path)
+    read_p = 'I://dataset//EUS_bulk//train//P//EUS2_p44_img_1.jpg'
+    randomwalk(read_p, './test_marker.jpg', './test_seg.jpg')
+    # for i, name in enumerate(filename):
+    #     read_path = f'I://dataset//EUS_bulk//train//P//{name}'
+    #     marker1_path = f'I://dataset//marker//marker_{name}'
+    #     seg_path = f'I://dataset//segment//segment_{name}'
+    #     randomwalk(read_path, marker1_path, seg_path)
 
 
